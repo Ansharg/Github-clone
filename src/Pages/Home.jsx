a90888186c40e5614ee9e1fbda9625cfc599cc0d
Ansharg <anshgarg94161@gmail.com>
2023-07-17 23:50:27 +0530

    here i have added the some part of github clone it is looking similar to it

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import bgwebsite from '../Asests/Images/bgwebsite.png'
 import Header from '../Components/Header'
 import {AiOutlineRight} from 'react-icons/ai'
+import {BsCode} from 'react-icons/bs'
 
 export default function Home() {
   return (
@@ -11,11 +12,15 @@ export default function Home() {
             <img src="https://github.githubassets.com/images/modules/site/home-campaign/hero-drone.webp" alt="space-ship" className='bg-transparent h-52 w-fit absolute right-20 ' style={{animation:"updown 10s infinite"}} />
         </div>
         <div className='bg-transparent flex'>
-            <div className='bg-transparent w-[35%]'>
-                <img src="https://github.githubassets.com/images/modules/site/home-campaign/lines-hero.svg" alt="svg" className='bg-transparent top-[43px] relative left-28 ' />
+            <div className='bg-transparent w-[50%]'>
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/lines-hero.svg" alt="svg" className='bg-transparent top-[50px] relative left-28 ' />
+                <div className='h-12 w-12 flex items-center justify-center relative top-16 text-3xl left-[6.5rem]' style={{background:"radial-gradient(49.69% 49.90% at 49.90% 49.69%, rgba(156, 109, 255, 0.69) 0%, rgba(147, 129, 185, 0.00) 100%)"}}>
+                    <BsCode className='text-white text-3xl bg-transparent' />
+                </div>
+                <div className='h-[28rem] bg-gradient-to-b from-[#6b43c1] to-green-500 w-1 rounded-full relative left-[7.8rem] top-20 '></div>
             </div>
-            <div className='bg-transparent mt-[23rem] relative'>
-                <div className='bg-transparent ml-[-320px]'>
+            <div className='bg-transparent mt-[23rem] ml-[-280px] relative'>
+                <div className='bg-transparent'>
                     <button className='flex rounded-full px-5 py-3 items-center bg-transparent border border-gray-600 space-x-6' style={{background: "radial-gradient(137.80% 80.58% at 49.90% 49.69%, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.00) 100%)"}}>
                         <img src="https://github.githubassets.com/images/modules/site/eyebrow-banner-icon-copilot-x.svg" alt="" className='bg-transparent h-[45px]' />
                         <div className='text-white bg-transparent'>
@@ -24,6 +29,25 @@ export default function Home() {
                         </div>
                         <AiOutlineRight className='text-white bg-transparent'/>
                     </button>
+                </div>
+                <div className=' mt-7 text-left bg-transparent space-y-2'>
+                    <p className='text-white text-[5.2rem] font-semibold bg-transparent'>Let's build from here</p>
+                    <p className='text-gray-500 text-4xl w-[70%] leading-10'>Harnessed for productivity. Designed for collaboration. Celebrated for built-in security. Welcome to the platform developers love.</p>
+                </div>
+                <div className='mt-16 flex items-center'>
+                    <input type="text" id='emailaddress' className='bg-white h-12 w-[25%] rounded-l-lg px-5' placeholder='Email address'/>
+                    <button className='bg-[#6b43c1] text-white h-12 px-5 rounded-r-lg font-bold'>Sign up for GitHub</button>
+                </div>
+                <div className='mt-[140px]'>
+                    <p className='text-gray-500 text-lg font-light'>Trusted by the world’s leading organizations </p>
+                    <div id='icons' className='flex space-x-16 items-center mt-8'>
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/stripe.svg" alt="stripe" className='h-12'/>
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pinterest.svg" alt="pinterest" className='h-12' />
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/kpmg.svg" alt="kpmg" className='h-12' />
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/mercedes.svg" alt="Mercedes Benz" className='h-12' />
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pg.svg" alt="P&G" className='h-8' />
+                        <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/telus.svg" alt="Telus" className='h-8' />
+                    </div>
                 </div>
             </div>
         </div>
